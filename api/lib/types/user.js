400839109fd95c55cc6424a79d9a230fdd348efc
ingalls <nick@ingalls.ca>
2022-11-21 17:09:45 -0700

    Add from_username

--- api/lib/types/user.js
+++ api/lib/types/user.js
@@ -1,5 +1,29 @@
 import Generic from '@openaddresses/batch-generic';
+import { sql } from 'slonik';
 
 export default class User extends Generic {
     static _table = 'users';
+
+    static async from_username(pool, username) {
+        let pgres;
+        try {
+            pgres = await pool.query(sql`
+                SELECT
+                    *
+                FROM
+                    users
+                WHERE
+                    username = ${username}
+                    OR email = ${username}
+            `);
+        } catch (err) {
+            throw new Err(500, err, 'Internal Login Error');
+        }
+
+        if (pgres.rows.length === 0) {
+            throw new Err(403, null, 'Invalid Username or Pass');
+        }
+
+        return this.deserialize(pool, pgres);
+    }
 }
