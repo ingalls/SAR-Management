636e5ff145ff6371a059e5b0bdf33c357e17677d
ingalls <nick@ingalls.ca>
2022-11-29 09:53:15 -0700

    Add support for basic filtering of issues

--- api/lib/types/issue.js
+++ api/lib/types/issue.js
@@ -1,5 +1,37 @@
-import Generic from '@openaddresses/batch-generic';
+import Generic, { Params } from '@openaddresses/batch-generic';
+import Err from '@openaddresses/batch-error';
+import { sql } from 'slonik';
 
 export default class Issue extends Generic {
     static _table = 'issues';
+
+    static async list(pool, query) {
+        query.limit = Params.integer(query.limit, { default: 20 });
+        query.page = Params.integer(query.page, { default: 0 });
+        query.filter = Params.string(query.filter);
+        query.sort = Params.string(query.sort, { default: 'created' });
+        query.order = Params.order(query.order);
+
+        try {
+            const pgres = await pool.query(sql`
+                SELECT
+                    count(*) OVER() AS count,
+                    *
+                FROM
+                    issues
+                WHERE
+                    (${query.filter}::TEXT IS NULL OR title ~* ${query.filter})
+                ORDER BY
+                    ${sql.identifier(['issues', query.sort])} ${query.order}
+                LIMIT
+                    ${query.limit}
+                OFFSET
+                    ${query.limit * query.page}
+            `);
+
+            return this.deserialize_list(pgres);
+        } catch (err) {
+            throw new Err(500, err, 'Failed to list assets');
+        }
+    }
 }
