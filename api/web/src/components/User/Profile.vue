32ff4c509cf82d43cf2e759d58c9c363ddf4ccfa
ingalls <nick@ingalls.ca>
2022-12-07 08:59:18 -0700

    Add User componentgit push

---
+++ api/web/src/components/User/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <img v-if='user.profile_id' :src='`${base}/api/asset/${user.profile_id}/raw?token=${token}`' style='width: 100%;'/>
+    <img v-else src='/user.webp' class='width: 100%;'/>
+</template>
+
+<script>
+export default {
+    name: 'UserProfile',
+    props: {
+        user: Object
+    },
+    data: function() {
+        return {
+            token: localStorage.token,
+            base: window.stdurl('/').origin,
+        }
+    },
+}
+</script>
