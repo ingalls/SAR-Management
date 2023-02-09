5952cb4a91a6f7510dea193e140db7b1b8ec0b8b
ingalls <nick@ingalls.ca>
2023-02-09 16:04:18 -0700

    Add Loading to homepage cards

--- api/web/src/components/cards/Issues.vue
+++ api/web/src/components/cards/Issues.vue
@@ -5,7 +5,10 @@
             <h3 class="card-title"><a @click='$router.push("/issue")' class='cursor-pointer' v-text='label'></a></h3>
         </div>
     </div>
-    <template v-if='!issues.length'>
+    <template v-if='loading'>
+        <TablerLoading desc='Loading Issues'/>
+    </template>
+    <template v-else-if='!issues.length'>
         <None :create='false' label='Assigned Issues' :compact='true'/>
     </template>
     <template v-else>
@@ -29,6 +32,9 @@
 
 <script>
 import None from '../util/None.vue';
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler'
 
 export default {
     name: 'IssueCard',
@@ -48,6 +54,7 @@ export default {
     },
     data: function() {
         return {
+            loading: true,
             issues: [],
         }
     },
@@ -56,15 +63,17 @@ export default {
     },
     methods: {
         fetch: async function() {
+            this.loading = true;
             const url = window.stdurl('/api/issue');
             url.searchParams.append('limit', this.limit);
             if (this.assigned) url.searchParams.append('assigned', this.assigned);
             this.issues = (await window.std(url)).issues;
+            this.loading = false;
         }
     },
     components: {
         None,
-
+        TablerLoading
     }
 }
 </script>
--- api/web/src/components/cards/Trainings.vue
+++ api/web/src/components/cards/Trainings.vue
@@ -5,23 +5,36 @@
             <h3 class="card-title"><a @click='$router.push("/training")' class='cursor-pointer' v-text='label'></a></h3>
         </div>
     </div>
-    <table class="table card-table table-vcenter">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th colspan="2">Labels</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr :key='training.id' v-for='training in list.training'>
-                <td><a @click='$router.push(`/training/${training.id}`)' v-text='training.title' class='cursor-pointer'></a></td>
-            </tr>
-        </tbody>
-    </table>
+    <template v-if='loading'>
+        <TablerLoading desc='Loading Trainings'/>
+    </template>
+    <template v-else-if='!list.training.length'>
+        <None :create='false' :label='training' :compact='true'/>
+    </template>
+    <template v-else>
+        <table class="table card-table table-vcenter">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th colspan="2">Labels</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key='training.id' v-for='training in list.training'>
+                    <td><a @click='$router.push(`/training/${training.id}`)' v-text='training.title' class='cursor-pointer'></a></td>
+                </tr>
+            </tbody>
+        </table>
+    </template>
 </div>
 </template>
 
 <script>
+import None from '../util/None.vue';
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler'
+
 export default {
     name: 'IssueCard',
     props: {
@@ -36,6 +49,7 @@ export default {
     },
     data: function() {
         return {
+            loading: true,
             list: {
                 total: 0,
                 training: []
@@ -47,10 +61,16 @@ export default {
     },
     methods: {
         fetch: async function() {
+            this.loading = true;
             const url = window.stdurl('/api/training');
             url.searchParams.append('limit', this.limit);
             this.list = await window.std(url);
+            this.loading = false;
         }
     },
+    components: {
+        TablerLoading,
+        None
+    }
 }
 </script>
