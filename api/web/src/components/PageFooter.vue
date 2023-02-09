5cbcae6e5260393d37ef6807ead2b8ead00098e3
ingalls <nick@ingalls.ca>
2023-02-09 14:39:55 -0700

    Add API Docs

--- api/web/src/components/PageFooter.vue
+++ api/web/src/components/PageFooter.vue
@@ -4,8 +4,8 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                  <li class="list-inline-item"><a href="./docs/" class="link-secondary">Documentation</a></li>
-                  <li class="list-inline-item"><a href="https://github.com/tak-ps/stats" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+                  <li class="list-inline-item"><a href="/docs/" class="link-secondary">Documentation</a></li>
+                  <li class="list-inline-item"><a href="https://github.com/ingalls/SAR-Management" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
                 </ul>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
