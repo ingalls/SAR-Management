d79615d7ef57161fdfd5d38e3ef2637dda2f8221
ingalls <nick@ingalls.ca>
2023-02-10 16:12:24 -0700

    Make avatar clickable

--- api/web/src/components/util/Avatar.vue
+++ api/web/src/components/util/Avatar.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
     <span class="avatar avatar-xs me-2 avatar-rounded" style="background-image: url(./static/avatars/000m.jpg)"></span>
-    <span v-if='user.name' v-text='`${user.name}`'/>
-    <span v-else v-text='`${user.fname} ${user.lname}`'/>
+    <span v-if='user.name'><a @click='$router.push(`/user/${user.uid || user.id}`)' class='cursor-pointer' v-text='user.name'></a></span>
+    <span v-else><a @click='$router.push(`/user/${user.uid || user.id}`)' class='cursor-pointer' v-text='`${user.fname} ${user.lname}`'></a></span>
 </div>
 </template>
 
