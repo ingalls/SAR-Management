d55abfe6b3851ed6830542dff022cc23f065f866
ingalls <nick@ingalls.ca>
2023-02-13 16:14:06 -0500

    Add NoAccess Modal

---
+++ api/web/src/components/util/NoAccess.vue
@@ -0,0 +1,37 @@
+<template>
+<div class='card'>
+    <div class='card-body'>
+        <div class='d-flex justify-content-center' :class='{
+            "mt-4 mb-2": !compact
+        }'>
+            <EyeOffIcon v-if='compact' width='32' height='32' />
+            <EyeOffIcon v-else width='48' height='48' />
+        </div>
+
+        <div class='text-center' :class='{
+            "mb-4 mt-2": !compact
+        }'>
+            <div>Insufficient Access</div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import {
+    EyeOffIcon
+} from 'vue-tabler-icons'
+
+export default {
+    name: 'NoAccess',
+    props: {
+        compact: {
+            type: Boolean,
+            default: false
+        }
+    },
+    components: {
+        EyeOffIcon
+    }
+}
+</script>
