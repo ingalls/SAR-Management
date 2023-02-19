d8eae3fa399979adc33da6c676b45f53f49f1f56
ingalls <nick@ingalls.ca>
2023-02-19 07:13:32 -0700

    Show container parents

--- api/lib/types/equipment.js
+++ api/lib/types/equipment.js
@@ -6,13 +6,14 @@ export default class Equipment extends Generic {
     static _table = 'equipment';
 
     static async list(pool, query) {
-        query.limit = Params.integer(query.limit, { default: 20 }); 
-        query.page = Params.integer(query.page, { default: 0 }); 
+        query.limit = Params.integer(query.limit, { default: 20 });
+        query.page = Params.integer(query.page, { default: 0 });
         query.filter = Params.string(query.filter);
-        query.sort = Params.string(query.sort, { default: 'created' }); 
+        query.sort = Params.string(query.sort, { default: 'created' });
         query.order = Params.order(query.order);
 
         query.assigned = Params.integer(query.assigned);
+        query.parent = Params.integer(query.parent);
         query.start = Params.timestamp(query.timestamp);
         query.end = Params.timestamp(query.timestamp);
 
@@ -26,7 +27,8 @@ export default class Equipment extends Generic {
                         LEFT JOIN equipment_assigned
                             ON equipment.id = equipment_assigned.equip_id
                 WHERE
-                    (${query.filter}::TEXT IS NULL OR name ~* ${query.filter})
+                    ((${query.parent}::BIGINT IS NULL AND container_parent IS NULL) OR (${query.parent}::BIGINT IS NOT NULL AND container_parent = ${query.parent}::BIGINT))
+                    AND (${query.filter}::TEXT IS NULL OR name ~* ${query.filter})
                     AND (${query.assigned}::BIGINT IS NULL OR equipment_assigned.uid = ${query.assigned})
                 ORDER BY
                     ${sql.identifier([this._table, query.sort])} ${query.order}
@@ -40,6 +42,6 @@ export default class Equipment extends Generic {
         } catch (err) {
             throw new Err(500, err, 'Failed to list Equipment');
         }
-    }   
+    }
 }
 
