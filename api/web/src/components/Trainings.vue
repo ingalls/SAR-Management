6e50e607ab62bee1dd4ee0af77cb20661b449f0f
ingalls <nick@ingalls.ca>
2023-03-08 11:32:55 -0700

    Add paging to training list

--- api/web/src/components/Trainings.vue
+++ api/web/src/components/Trainings.vue
@@ -26,11 +26,12 @@
             <div class='row row-deck row-cards'>
                 <div class="col-lg-12">
                     <NoAccess v-if='!is_iam("Training:View")' title='Trainings'/>
+                    <TablerLoading v-else-if='loading.list'/>
                     <div v-else class="card">
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="input-icon w-50">
-                                    <input v-model='query.filter' type="text" class="form-control" placeholder="Search…">
+                                    <input v-model='paging.filter' type="text" class="form-control" placeholder="Search…">
                                     <span class="input-icon-addon">
                                         <SearchIcon width='24'/>
                                     </span>
@@ -56,6 +57,7 @@
                         <template v-if='!list.total'>
                             <None label='Trainings' :create='false'/>
                         </template>
+                        <TableFooter :limit='paging.limit' :total='list.total' @page='paging.page = $event'/>
                     </div>
                 </div>
             </div>
@@ -72,6 +74,10 @@ import NoAccess from './util/NoAccess.vue';
 import None from './util/None.vue';
 import EpochRange from './util/EpochRange.vue';
 import PageFooter from './PageFooter.vue';
+import TableFooter from './util/TableFooter.vue';
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler'
 import {
     SearchIcon
 } from 'vue-tabler-icons';
@@ -90,8 +96,13 @@ export default {
     },
     data: function() {
         return {
-            query: {
-                filter: ''
+            paging: {
+                filter: '',
+                limit: 100,
+                page: 0
+            },
+            loading: {
+                list: true
             },
             list: {
                 total: 0,
@@ -100,9 +111,15 @@ export default {
         }
     },
     watch: {
-        'query.filter': async function() {
-            await this.listTrainings();
-        }
+       'paging.page': async function() {
+           await this.listTrainings();
+       },
+       'paging.filter': async function() {
+           await this.listTrainings();
+       },
+       'paging.prefix': async function() {
+           await this.listTrainings();
+       }
     },
     mounted: async function() {
         if (this.is_iam("Training:View")) await this.listTrainings();
@@ -110,17 +127,24 @@ export default {
     methods: {
         is_iam: function(permission) { return iam(this.iam, this.auth, permission) },
         listTrainings: async function() {
+            this.loading.list = true;
             const url = window.stdurl('/api/training');
-            if (this.query.filter) url.searchParams.append('filter', this.query.filter);
+            url.searchParams.append('limit', this.paging.limit);
+            url.searchParams.append('page', this.paging.page);
+            url.searchParams.append('filter', this.paging.filter);
+            url.searchParams.append('order', 'desc');
             this.list = await window.std(url)
+            this.loading.list = false;
         }
     },
     components: {
         None,
+        TableFooter,
         EpochRange,
         SearchIcon,
         PageFooter,
-        NoAccess
+        NoAccess,
+        TablerLoading
     }
 }
 </script>
