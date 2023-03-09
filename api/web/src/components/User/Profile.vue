8a638bc11c967e3cbe4c36a86b6d0cb01cc8a099
ingalls <nick@ingalls.ca>
2023-03-09 14:21:01 -0700

    centre

--- api/web/src/components/User/Profile.vue
+++ api/web/src/components/User/Profile.vue
@@ -4,8 +4,8 @@
         <div v-else style='background-image: url(/user.webp); width: 100%; height: 400px; background-size: contain; background-repeat: no-repeat;'></div>
     </template>
     <template v-else>
-        <div v-if='user.profile_id' :style='`background-image: url(${base}/api/asset/${user.profile_id}/raw?token=${token});`' style='width: 100%; height: 400px; background-size: cover'></div>
-        <div v-else style='background-image: url(/user.webp); width: 100%; height: 400px; background-size: cover;'></div>
+        <div v-if='user.profile_id' :style='`background-image: url(${base}/api/asset/${user.profile_id}/raw?token=${token});`' style='width: 100%; height: 400px; background-size: cover; background-position: center;'></div>
+        <div v-else style='background-image: url(/user.webp); width: 100%; height: 400px; background-size: cover; background-position: center;'></div>
     </template>
 </template>
 
