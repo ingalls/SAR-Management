b7730a85eb2ad0ffd3b1c8013793ea339ac066ef
ingalls <nick@ingalls.ca>
2023-03-09 14:10:44 -0700

    Better profile pic

--- api/web/src/components/User/Profile.vue
+++ api/web/src/components/User/Profile.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if='user.profile_id' :style='`background-image: url(${base}/api/asset/${user.profile_id}/raw?token=${token});`' style='width: 100%; height: 400px; background-size: cover;'></div>
-    <div v-else style='background-image: url(/user.webp); width: 100%; height: 400px; background-size: cover;'></div>
+    <div v-else style='background-image: url(/user.webp); width: 100%; height: 400px; background-size: contain; background-repeat: no-repeat;'></div>
 </template>
 
 <script>
