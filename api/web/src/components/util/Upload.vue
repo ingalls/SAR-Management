62e84191940c8419dbe7400caab09e81b6479786
ingalls <nick@ingalls.ca>
2023-04-15 15:15:03 -0600

    Sketch out nicer upload modal

--- api/web/src/components/util/Upload.vue
+++ api/web/src/components/util/Upload.vue
@@ -3,28 +3,19 @@
         <button type="button" class="btn-close" @click='close' aria-label="Close"></button>
             <div class="modal-status bg-yellow"></div>
             <div class="modal-body text-center py-4">
-                <form class="dropzone dz-clickable" id="dropzone-default" action="./" autocomplete="off" novalidate="">
-                    <div class="dz-default dz-message">
-                        <button class="dz-button" type="button">Drop files here to upload</button>
-                    </div>
-                </form>
+                <UploadDefault
+                    :url='url'
+                />
             </div>
     </TablerModal>
 </template>
 
 <script>
 import { TablerModal } from '@tak-ps/vue-tabler';
-import Dropzone from '@tabler/core/dist/libs/dropzone/dist/dropzone.mjs';
-import '@tabler/core/dist/libs/dropzone/dist/dropzone.css';
-import '@tabler/core/dist/css/tabler-vendors.min.css';
+import UploadDefault from './UploadDefault.vue';
 
 export default {
     name: 'UploadModal',
-    data: function() {
-        return {
-            dropzone: null
-        }
-    },
     props: {
         url: {
             type: String,
@@ -35,30 +26,6 @@ export default {
             default: ''
         }
     },
-    mounted: function() {
-        this.$nextTick(() => {
-            this.dropzone = new Dropzone("#dropzone-default", {
-                autoProcessQueue: false
-            });
-
-            this.dropzone.on('addedfile', async (file) => {
-                const body = new FormData();
-                body.append('file', file);
-
-                try {
-                    const url = window.stdurl(this.url);
-                    if (this.prefix) url.searchParams.append('prefix', this.prefix);
-
-                    this.$emit('upload', await window.std(url, {
-                        method: 'POST',
-                        body
-                    }));
-                } catch (err) {
-                    this.$emit('err', err);
-                }
-            });
-        });
-    },
     methods: {
         close: function() {
             this.$emit('close');
@@ -66,6 +33,7 @@ export default {
     },
     components: {
         TablerModal,
+        UploadDefault
     }
 }
 </script>
