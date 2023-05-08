bb06c1eecef8b7d8932113e82541682591a5a7c5
ingalls <nick@ingalls.ca>
2023-05-08 15:31:09 -0600

    Fix Birthday URL bug

--- api/routes/calendar.js
+++ api/routes/calendar.js
@@ -102,7 +102,7 @@ export default async function router(schema, config) {
                         events.push({
                             title: `${user.fname} ${user.lname.slice(0, 1)}'s B-Day`,
                             start: moment(query.start).year() + '-' + moment(user.bday).format('MM-DD'),
-                            path: `/team/user/${user.id}`,
+                            path: `/user/${user.id}`,
                             end: moment(query.start).year() + '-' + moment(user.bday).format('MM-DD')
                         });
                     }
