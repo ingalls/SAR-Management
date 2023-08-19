898fe364c91a3809c6ac849d7865f4258a2e618b
ingalls <nick@ingalls.ca>
2023-08-19 12:06:44 -0600

    Add Mission View Migration

--- api/lib/types/mission.js
+++ api/lib/types/mission.js
@@ -51,4 +51,30 @@ export default class Mission extends Generic {
             throw new Err(500, err, 'Failed to list Missions');
         }
     }
+
+    async delete(opts = {}) {
+        try {
+            await this._pool.query(sql`
+                DELETE FROM missions_team
+                    WHERE
+                        mission_id = ${this.id}
+            `);
+
+            await this._pool.query(sql`
+                DELETE FROM missions_assigned
+                    WHERE
+                        mission_id = ${this.id}
+            `);
+
+            await this._pool.query(sql`
+                DELETE FROM ${sql.identifier([this._table])}
+                    WHERE
+                        id = ${this.id}
+            `);
+
+            return true;
+        } catch (err) {
+            throw new Err(500, new Error(err), `Failed to delete from ${this._table}`);
+        }
+    }
 }
