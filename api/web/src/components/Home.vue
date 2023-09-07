76aa19477d575ad8af58e863b275196998f7dc9f
ingalls <nick@ingalls.ca>
2023-09-07 19:57:56 +0100

    Draggable Cards

--- api/web/src/components/Home.vue
+++ api/web/src/components/Home.vue
@@ -2,22 +2,24 @@
 <div>
     <div class='page-body'>
         <div class='container-xl'>
-            <div class='row row-deck row-cards'>
-                <div class="col-lg-6">
-                    <IssuesCard
-                        v-if='is_iam("Issue:View")'
-                        :limit='5'
-                        :iam='iam'
-                        :auth='auth'
-                    />
-                    <NoAccess title='Recent Issues' v-else/>
-                </div>
-
-                <div class="col-lg-6">
-                    <TrainingsCard v-if='is_iam("Training:View")' :limit='5'/>
-                    <NoAccess title='Upcoming Trainings' v-else/>
-                </div>
-            </div>
+            <Draggable v-model="cards" itemKey='id' class='row row-cards'>
+                <template #item="{element}">
+                    <div class='col-lg-6' v-if='element.name === "Issues"'>
+                        <IssuesCard
+                            v-if='is_iam("Issue:View")'
+                            :limit='5'
+                            :iam='iam'
+                            :auth='auth'
+                            :drag='true'
+                        />
+                        <NoAccess title='Recent Issues' v-else/>
+                    </div>
+                    <div class="col-lg-6" v-else-if='element.name === "Trainings"'>
+                        <TrainingsCard v-if='is_iam("Training:View")' :limit='5'/>
+                        <NoAccess title='Upcoming Trainings' v-else/>
+                    </div>
+                </template>
+            </Draggable>
         </div>
     </div>
 </div>
@@ -28,6 +30,7 @@ import iam from '../iam.js';
 import IssuesCard from './cards/Issues.vue';
 import TrainingsCard from './cards/Trainings.vue';
 import NoAccess from './util/NoAccess.vue';
+import Draggable from 'vuedraggable';
 
 export default {
     name: 'Home',
@@ -41,10 +44,22 @@ export default {
             required: true
         }
     },
+    data: function() {
+        return {
+            cards: [{
+                id: 1,
+                name: 'Issues'
+            },{
+                id: 2,
+                name: 'Trainings'
+            }]
+        }
+    },
     methods: {
         is_iam: function(permission) { return iam(this.iam, this.auth, permission) }
     },
     components: {
+        Draggable,
         IssuesCard,
         TrainingsCard,
         NoAccess
