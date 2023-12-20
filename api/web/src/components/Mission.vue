b507fa3503ee9cb16bf8026903a276dc3ece75cb
ingalls <nick@ingalls.ca>
2023-12-20 14:25:27 -0700

    Add Personell Count & Man hours

--- api/web/src/components/Mission.vue
+++ api/web/src/components/Mission.vue
@@ -61,6 +61,10 @@
 
                                     <div class='col-12 datagrid'>
                                         <div class="datagrid-item">
+                                            <div class="datagrid-title">Personnel</div>
+                                            <div class="datagrid-content" v-text='mission.users.length'></div>
+                                        </div>
+                                        <div class="datagrid-item">
                                             <div class="datagrid-title">Man-Hours</div>
                                             <div class="datagrid-content" v-text='Math.round(mission.users.length * (mission.end_ts - mission.start_ts) / 1000 / 60 / 60)'></div>
                                         </div>
