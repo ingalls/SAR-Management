75e90306a1ea342fc5c566c2a2f0fb41311d5d6d
ingalls <nick@ingalls.ca>
2025-02-16 11:23:35 -0700

    Add UID to Assets TAble

--- api/migrations/0009_magical_dragon_lord.sql
+++ api/migrations/0009_magical_dragon_lord.sql
@@ -1,2 +1,6 @@
-ALTER TABLE "assets" ADD COLUMN "uid" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "assets" ADD COLUMN "uid" integer;--> statement-breakpoint
 ALTER TABLE "assets" ADD CONSTRAINT "assets_uid_users_id_fk" FOREIGN KEY ("uid") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+
+UPDATE assets SET uid = 1;
+
+ALTER TABLE "assets" ALTER COLUMN "uid" SET NOT NULL;
