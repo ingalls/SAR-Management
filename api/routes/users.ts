88c440d54d8b492e2f68c314654be340a619e689
ingalls <nick@ingalls.ca>
2025-02-24 13:20:22 -0700

    Allow Search By Email

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -70,7 +70,11 @@ export default async function router(schema: Schema, config: Config) {
                         page: page,
                         limit: 100,
                         where: sql`
-                            (${Param(req.query.filter)}::TEXT IS NULL OR fname||' '||lname ~* ${Param(req.query.filter)})
+                            (
+                                ${Param(req.query.filter)}::TEXT IS NULL
+                                OR  fname||' '||lname ~* ${Param(req.query.filter)})
+                                OR email  ~* ${Param(req.query.filter)})
+                            )
                             AND (${Param(req.query.team)}::INT IS NULL OR teams_id @> ARRAY[${Param(req.query.team)}::INT])
                             AND (${Param(req.query.disabled)}::BOOLEAN IS NULL OR users.disabled = ${Param(req.query.disabled)})
                         `
