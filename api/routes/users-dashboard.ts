24df42c346057d5430216f4bdef6d22893783d37
ingalls <nick@ingalls.ca>
2025-10-06 07:57:54 -0600

    Add PWA

---
+++ api/routes/users-dashboard.ts
@@ -0,0 +1,138 @@
+import Err from '@openaddresses/batch-error';
+import { GenericListOrder } from '@openaddresses/batch-generic';
+import { sql } from 'drizzle-orm';
+import Auth, { PermissionsLevel, IamGroup } from '../lib/auth.js';
+import Schema from '@openaddresses/batch-schema';
+import Config from '../lib/config.js';
+import Spaces from '../lib/aws/spaces.js';
+import { Type } from '@sinclair/typebox';
+import { StandardResponse, UserDashboardResponse } from '../lib/types.js';
+import { UserDashboard } from '../lib/schema.js';
+
+export default async function router(schema: Schema, config: Config) {
+    const spaces = new Spaces();
+
+    await schema.get('/user/:userid/dashboard', {
+        name: 'Get Dashboard',
+        group: 'Dashboard',
+        description: 'Get Dashboard Cards',
+        params: Type.Object({
+            userid: Type.Integer(),
+        }),
+        query: Type.Object({
+            limit: Type.Optional(Type.Integer()),
+            page: Type.Optional(Type.Integer()),rder: Type.Optional(Type.Enum(GenericListOrder)),
+            order: Type.Optional(Type.Enum(GenericListOrder)),
+            sort: Type.Optional(Type.String({default: 'id', enum: Object.keys(UserDashboard)})),
+            filter: Type.Optional(Type.String({ default: '' }))
+        }),
+        res: Type.Object({
+            total: Type.Integer(),
+            items: Type.Array(UserDashboardResponse)
+        })
+    }, async (req, res) => {
+        try {
+            await Auth.is_iam(config, req, IamGroup.User, PermissionsLevel.VIEW);
+
+            res.json(await config.models.Cert.list({
+                limit: req.query.limit,
+                page: req.query.page,
+                order: req.query.order,
+                sort: req.query.sort,
+                where: sql`
+                    uid = ${req.params.userid}
+                `
+            }));
+        } catch (err) {
+             Err.respond(err, res);
+        }
+    });
+
+    /*
+    await schema.get('/user/:userid/cert/:certid', {
+        name: 'Get Cert',
+        group: 'Cert',
+        description: 'Get a cert for the given user',
+        params: Type.Object({
+            userid: Type.Integer(),
+            certid: Type.Integer()
+        }),
+        res: CertResponse
+    }, async (req, res) => {
+        try {
+            await Auth.is_iam(config, req, IamGroup.User, PermissionsLevel.VIEW);
+
+            const cert = await config.models.Cert.from(req.params.certid);
+            if (cert.uid !== req.params.userid) throw new Err(400, null, 'Mismatch between UserID and Cert');
+
+            res.json(cert);
+        } catch (err) {
+             Err.respond(err, res);
+        }
+    });
+
+    await schema.delete('/user/:userid/cert/:certid', {
+        name: 'Delete Cert',
+        group: 'Cert',
+        description: 'Delete a cert for the given user',
+        params: Type.Object({
+            userid: Type.Integer(),
+            certid: Type.Integer()
+        }),
+        res: StandardResponse
+    }, async (req, res) => {
+        try {
+            await Auth.is_own_or_iam(config, req, req.params.userid, IamGroup.User, PermissionsLevel.MANAGE);
+
+            const cert = await config.models.Cert.from(req.params.certid);
+            if (cert.uid !== req.params.userid) throw new Err(400, null, 'Mismatch between UserID and Cert');
+
+            const asset = await config.models.Asset.from(cert.asset);
+
+            await spaces.delete({
+                Key: `assets/${asset.id}-${asset.name}`
+            });
+
+            config.models.Cert.delete(req.params.certid)
+
+            res.json({
+                status: 200,
+                message: 'Asset Deleted'
+            });
+        } catch (err) {
+             Err.respond(err, res);
+        }
+    });
+
+    await schema.post('/user/:userid/cert', {
+        name: 'Create Certs',
+        group: 'Cert',
+        description: 'Create a new Certificate',
+        params: Type.Object({
+            userid: Type.Integer(),
+        }),
+        body: Type.Object({
+            name: Type.String(),
+            asset: Type.Integer(),
+            known: Type.Optional(Type.Integer()),
+            expiry: Type.Optional(Type.String({
+                format: 'date'
+            }))
+        }),
+        res: CertResponse
+    }, async (req, res) => {
+        try {
+            const user = await Auth.is_own_or_iam(config, req, req.params.userid, IamGroup.User, PermissionsLevel.MANAGE);
+
+            const cert = await config.models.Cert.generate({
+                uid: user.id,
+                ...req.body
+            });
+
+            res.json(cert);
+        } catch (err) {
+             Err.respond(err, res);
+        }
+    });
+*/
+}
--- api/web/vite.config.js
+++ api/web/vite.config.js
@@ -1,15 +1,54 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'node:path';
 import vue from '@vitejs/plugin-vue'
+import icons from './public/logos/icons.ts';
 
-export default defineConfig({
-    plugins: [
-        vue()
-    ],
-    optimizeDeps: {
-        include: ["showdown"],
-    },
-    server: {
-        port: 8080,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const res = {
+        plugins: [
+            vue(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                manifest: {
+                    name: 'MesaSAR',
+                    short_name: 'MesaSAR',
+                    description: 'MesaSAR Team Management',
+                    theme_color: '#000000',
+                    icons
+                },
+                devOptions: {
+                    enabled: true
+                },
+                workbox: {
+                    maximumFileSizeToCacheInBytes: 3000000,
+                    clientsClaim: true,
+                    skipWaiting: true
+                }
+            })
+        ],
+        optimizeDeps: {
+            include: ["showdown", "@tak-ps/vue-tabler"],
+        },
+        build: {
+            rollupOptions: {
+                input: {
+                    main: path.resolve(__dirname, 'index.html'),
+                },
+                output: {
+                    entryFileNames: `assets/[name].js`,
+                    chunkFileNames: `assets/[name].js`,
+                    assetFileNames: `assets/[name].[ext]`
+                }
+            },
+        },
+        server: {
+            port: 8080,
+        },
     }
+
+    return res;
 })
 
