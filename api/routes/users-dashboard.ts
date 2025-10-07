b362b29d2c46101bcc57df1e0242d95cbcf497a8
ingalls <nick@ingalls.ca>
2025-10-07 08:22:20 -0600

    Fix Build

--- api/routes/users-dashboard.ts
+++ api/routes/users-dashboard.ts
@@ -34,7 +34,7 @@ export default async function router(schema: Schema, config: Config) {
         try {
             await Auth.is_iam(config, req, IamGroup.User, PermissionsLevel.VIEW);
 
-            res.json(await config.models.Cert.list({
+            res.json(await config.models.UserDashboard.list({
                 limit: req.query.limit,
                 page: req.query.page,
                 order: req.query.order,
