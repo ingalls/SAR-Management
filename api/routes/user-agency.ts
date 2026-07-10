186597cdbdb1a460d814723107cf1afb3393f6cf
ingalls <nick@ingalls.ca>
2026-07-10 08:22:21 -0600

    Is Auth

--- api/routes/user-agency.ts
+++ api/routes/user-agency.ts
@@ -23,7 +23,7 @@ export default async function router(schema: Schema, config: Config) {
         })
     }, async (req, res) => {
         try {
-            await Auth.is_user(config, req);
+            await Auth.is_auth(config, req);
 
             const result = await config.models.UserAgency.listByUser(req.params.userid);
 
